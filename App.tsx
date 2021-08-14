24b8ba37fa9a817ce5ae41c367b4087ca41a151f
jakub-bartosik-apptimia <jakub.bartosik@apptimia.com>
2021-08-14 23:24:54 +0200

    first changes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState<number>(0);
+  const [text, setText] = useState<string>("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.header}>
+        <Button title="Add" onPress={() => setCount(count + 1)} />
+        <Button title="remove" onPress={() => setCount(count - 1)} />
+      </View>
+      <Text style={styles.button}>Result: {count}</Text>
+      <View style={styles.header}>
+        <Text>Enter:</Text>
+        <TextInput
+          multiline
+          keyboardType="numeric"
+          placeholder="type.."
+          style={styles.input}
+          onChangeText={(text) => setText(text)}
+        />
+        <Text style={styles.button}>Text: {text}</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +33,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+    width: "100%",
+  },
+  button: {
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "white",
+    padding: 5,
+    borderColor: "black",
   },
 });
