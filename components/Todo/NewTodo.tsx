641e1274b5b87d8008eb94a31f7a1978a18253c3
jakub.bartosik@mayeryn.com <jbartos13@gmail.com>
2021-09-08 19:16:10 +0200

    added apollo & connected to gql

--- components/Todo/NewTodo.tsx
+++ components/Todo/NewTodo.tsx
@@ -1,17 +1,87 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
-import { Text, View } from "react-native";
+import { FlatList, Text, View, StyleSheet } from "react-native";
 import { NavigationStackProp } from "react-navigation-stack";
+
+type MISSIONS_RESULT_QUERY = {
+  launchesPast: Array<{
+    id: string;
+    mission_name: string;
+    rocket: {
+      rocket_name: string;
+    };
+    ships: Array<{
+      name: string;
+      id: string;
+    }>;
+  }>;
+};
+
+const GET_MISSIONS_QUERY = gql`
+  query {
+    launchesPast(limit: 2) {
+      id
+      mission_name
+      rocket {
+        rocket_name
+      }
+      ships {
+        name
+        id
+      }
+    }
+  }
+`;
 
 export interface NewTodoProps {
   navigation: NavigationStackProp;
 }
 
 const NewTodo: React.FC<NewTodoProps> = ({ navigation }) => {
+  const { loading, data, error } =
+    useQuery<MISSIONS_RESULT_QUERY>(GET_MISSIONS_QUERY);
+  console.log("loading", loading);
+  console.log("data", data);
+  console.log("error", error);
+  console.log("result", data?.launchesPast);
+
+  if (loading) {
+    return (
+      <View>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
+
   return (
     <View>
       <Text>New TODO</Text>
+      <FlatList
+        data={data?.launchesPast}
+        renderItem={({ item: mission }) => (
+          <View key={mission.id}>
+            <Text style={style.itemText}>{mission.mission_name}</Text>
+            <Text>{mission.rocket.rocket_name}</Text>
+            <FlatList
+              data={mission.ships}
+              renderItem={({ item: ship }) => (
+                <View key={ship.id}>
+                  <Text>{ship.name}</Text>
+                </View>
+              )}
+            />
+          </View>
+        )}
+      />
     </View>
   );
 };
 
 export default NewTodo;
+
+const style = StyleSheet.create({
+  itemText: {
+    marginLeft: 20,
+    fontFamily: "nunito-regular",
+  },
+});
--- components/Todo/TodoScreen.tsx
+++ components/Todo/TodoScreen.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import NewTodo from "./NewTodo";
 import Todo from "./Todo";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -12,28 +13,34 @@ export enum TODO_ROUTES {
 const BottomTab = createBottomTabNavigator();
 
 export default function TodoScreen() {
+  const client = new ApolloClient({
+    uri: "https://api.spacex.land/graphql",
+    cache: new InMemoryCache(),
+  });
   return (
-    <BottomTab.Navigator initialRouteName={TODO_ROUTES.NEW_TODO}>
-      <BottomTab.Screen
-        name={TODO_ROUTES.TODO}
-        component={Todo}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <Entypo name="list" size={size} color={color} />
-          ),
-        }}
-      />
-      <BottomTab.Screen
-        name={TODO_ROUTES.NEW_TODO}
-        component={NewTodo}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <Entypo name="new" size={size} color={color} />
-          ),
-        }}
-      />
-    </BottomTab.Navigator>
+    <ApolloProvider client={client}>
+      <BottomTab.Navigator initialRouteName={TODO_ROUTES.NEW_TODO}>
+        <BottomTab.Screen
+          name={TODO_ROUTES.TODO}
+          component={Todo}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <Entypo name="list" size={size} color={color} />
+            ),
+          }}
+        />
+        <BottomTab.Screen
+          name={TODO_ROUTES.NEW_TODO}
+          component={NewTodo}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <Entypo name="new" size={size} color={color} />
+            ),
+          }}
+        />
+      </BottomTab.Navigator>
+    </ApolloProvider>
   );
 }
